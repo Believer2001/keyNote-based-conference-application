58f599d9ead70316be136dba2051c5515a6ab5df
Believer2001 <tsehbenoit2001@gmail.com>
2025-10-20 10:22:09 +0100

    creation des autrees service

---
+++ configuration-service/src/test/java/enset/ma/configurationservice/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.ma.configurationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
